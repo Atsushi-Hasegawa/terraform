e7d6a72b8fdd450641b7f5e3ff2fe6f335874540
Hasegawa <hasegawa@HasegawanoMacBook-Air.local>
2026-05-30 11:21:39 +0900

    style(aws): outputs.tf を分離し、ディレクトリ構成の整理を完了
    各環境（common/staging）において main.tf から出力定義を outputs.tf に切り出しました。
    これにより、入力(variables)、内部ロジック(locals)、構成(main)、出力(outputs)の役割分離が完了しました。

--- aws/services/staging/outputs.tf
+++ aws/services/staging/outputs.tf
@@ -1,3 +1,4 @@
+# Athena Outputs
 output "athena_workgroup_name" {
   value = module.athena.athena_workgroup_name
 }
@@ -16,4 +17,14 @@ output "athena_glue_table_name" {
 
 output "athena_firehose_delivery_stream_arn" {
   value = module.athena.firehose_delivery_stream_arn
+}
+
+# Application Outputs
+output "alb_dns_name" {
+  description = "The DNS name of the application load balancer"
+  value       = module.app-lb.dns_name
+}
+
+output "ecs_cluster_name" {
+  value = module.ecs-app.cluster_name
 }
